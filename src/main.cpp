4f1874076fc4c6cc7748267839bd3b6ec96525e6
quanmcvn <59592286+quanmcvn@users.noreply.github.com>
2023-02-15 16:56:23 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <memory>
+#include "qbskr/main.hpp"
+
+static std::unique_ptr<Main> g_main;
+
+int main(int argc, char** argv) {
+	g_main = std::make_unique<Main>();
+	int ret = g_main->run(argc, argv);
+
+	return ret;
+}
---
+++ src/qbskr/main.hpp
@@ -0,0 +1,9 @@
+#ifndef HEADER_QBSKR_QBSKR_MAIN_HPP
+#define HEADER_QBSKR_QBSKR_MAIN_HPP
+
+class Main final {
+public:
+	int run(int argc, char** argv) ;
+};
+
+#endif
