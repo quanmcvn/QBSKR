06a42c026fb295104ccc7ade6d05ddf018a09caf
quanmcvn <59592286+quanmcvn@users.noreply.github.com>
2023-03-10 14:06:10 +0700

    apparently I didn't understand the SDL_BUTTON() macro

--- src/control/mouse_button_config.cpp
+++ src/control/mouse_button_config.cpp
@@ -7,7 +7,7 @@
 MouseButtonConfig::MouseButtonConfig() :
 	m_mouse_button_map()
 {
-	m_mouse_button_map[SDL_BUTTON(SDL_BUTTON_LEFT)] = { 0, Control::ATTACK };
+	m_mouse_button_map[SDL_BUTTON_LEFT] = { 0, Control::ATTACK };
 }
 
 SDL_Keycode MouseButtonConfig::reversemap_mousebutton(int player, Control control) const
